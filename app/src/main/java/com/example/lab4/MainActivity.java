a8239bc9ff53c93d4bb3b49e4f4b017e5d2a7e69
yyyifffang <Fragrant115@gmail.com>
2022-10-21 10:05:13 +0800

    first commit

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.lab4;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //宣告元件
+    private Button btn;
+    private TextView tv_meal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //連接畫面元件
+        tv_meal=findViewById(R.id.tv_meal);
+        //選擇按鈕點擊監聽
+        btn =findViewById(R.id.btn_choice);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //透過startActivityForResult發出Intent，並記錄請求對象
+                startActivityForResult(new Intent( MainActivity.this,MainActivity2.class),  1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null) return;
+        //驗證發出的對象回傳狀態
+        if(requestCode == 1) {
+            if(resultCode==101){
+                //讀取bundle中的資料
+                Bundle b =data.getExtras();
+                String str1 = b.getString( "drink");
+                String str2 = b.getString("sugar");
+                String str3 = b.getString("ice");
+                tv_meal.setText(String.format("飲料：%s\n\n甜度：%s\n\n冰塊：%s\n\n",str1,str2,str3));
+
+            }
+        }
+
+    }
+}
